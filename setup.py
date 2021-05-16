1b7ebe9851ade1c1a618761c95f9c6c5f4028441
Simon Vreugdenhil <simonvreugdenhil@gmail.com>
2021-05-16 12:09:53 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stock-trend-comparison',
-    version='0.1.0',
+    version='0.1.1',
     description='Python package used to compare stock trend performances',
     author='Simon Vreugdenhil',
     author_email='simon@simonvreugdenhil.com',
